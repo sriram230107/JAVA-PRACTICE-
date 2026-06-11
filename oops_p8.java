d507f2a33c7516eb5bf2ca11419c5ee698160f9e
SRI RAM B <rsbb7703@gmail.com>
2026-06-11 20:11:11 +0530

    corrected leak

--- oops_p8.java
+++ oops_p8.java
@@ -22,10 +22,11 @@ public class oops_p8
    public static void main(String[] args)
    {
      Hi obj=new Hi();
-    Scanner sc = new Scanner(System.in);
+     Scanner sc = new Scanner(System.in);
      int a=sc.nextInt();
      String b=sc.next();
      String c=sc.next();
      obj.show(a,b,c);
+     sc.close();
    }
 }
